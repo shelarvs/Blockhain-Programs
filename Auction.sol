f372a88fefe2602cb11cb731e4c842adce6e2840
Viraj Shelar <55134977+shelarvs@users.noreply.github.com>
2021-02-13 20:38:18 +0530

    Update Auction.sol

--- Auction.sol
+++ Auction.sol
@@ -1,108 +1,96 @@
 pragma solidity ^0.8.0;
 
-interface database{
-    function playerRegistration(address _player_id, uint _base_price, uint _age, string memory _player_type) external;
-    function teamRegistration(address _team_id, string memory _team_name) external;
+contract database{
     
-    function player_details(address _return_player_id) external view returns(address,uint,uint,string memory);
-    function team_details(address _return_team_id) external view returns(address,uint,string memory);
+    uint count=0;
     
-    function get_player_id(address _return_player_id) external view returns(address);
-    function get_player_base_price(address _return_player_id) external view returns(uint);
-    function get_player_age(address _return_player_id) external view returns(uint);
-    function get_player_type(address _return_player_id) external view returns(string memory);
-    
-    function get_team_id(address _return_team_id) external view returns(address);
-    function get_player_count(address _return_team_id) external view returns(uint);
-    function get_team_name(address _return_team_id) external view returns(string memory);
-    function get_team_players_selected(address _return_team_id) external view returns(address);
-    
-    function set_selected_player(address team_id ,address _selected_player_id) external;
-    
-}
-
-contract Auction{
-    address player_id_on_bid;
-    
-    uint bid_up_by=0;
-    
-    address bidder_data;
-    uint bidder_amount;
-    uint bidder_count=0;
-    bool isBidOn=false;
-    
-    uint previous_bid_value=0;
-    
-    address auction_manager;
-    
-    constructor () public payable  {
-        auction_manager = msg.sender;
+    struct player{
+        address player_id;
+        uint base_price;
+        uint age;
+        string player_type;
     }
+    player mp;
     
-    database db;
-    
-    function set_DB(address db_address) public returns(address){
-        require(msg.sender==auction_manager,"You are not Auction manager");
-        db=database(db_address);
-        return db_address;
+    struct team{
+        address team_id;
+        uint player_count;
+        string team_name;
+        address[] team_players;
     }
+    team mt;
     
-    function register_player(uint _base_price, uint _age, string memory _player_type)public{
-        db.playerRegistration(msg.sender,_base_price,_age,_player_type);
-    }
-    
-    function register_team(string memory _team_name)public{
-        db.teamRegistration(msg.sender,_team_name);
-    }
-
-    function get_player_details(address _player_address) public view returns(address,uint,uint,string memory){
-        return db.player_details(_player_address);
-    }
-    
-     function get_team_details(address _team_address) public view returns(address,uint,string memory){
-        return db.team_details(_team_address);
-    }
-    
-    function start_bid(address _player_id_on_bid) public{
-        require(msg.sender==auction_manager,"You are not Auction manager");
-        require(bid_up_by>0,"Setup Bid Up Value");
-        player_id_on_bid = _player_id_on_bid;
-        isBidOn=true;
-    }
-    
-    function bid_up(uint _bid_up_by) public{
-        bid_up_by = _bid_up_by;
-    }
-    
-    function bid()public payable{
-        require(isBidOn==true,"No Player on Bid");
-        if(bidder_count<=5){
-            if(previous_bid_value<msg.value){
-                payable(bidder_data).transfer(bidder_amount);
-                
-                bidder_data = msg.sender;
-                bidder_amount = msg.value;
-                bidder_count +=1;
-                previous_bid_value=msg.value + bid_up_by;   
-            }
-        }
-        else{
-            //db.set_selected_player(bidder_data, player_id_on_bid);
-            payable(player_id_on_bid).transfer(bidder_amount);
-            bidder_count=0;
-            isBidOn=false;
-        }
-    }
-    
-    function players_selected(address _team_address) public view returns(address){
-        return db.get_team_players_selected(_team_address);
-    }
-    
-    function Last_bid() public view returns(address,uint){
-        return (bidder_data,previous_bid_value);
-    }
-    
-      function player_on_bid_id() public view returns(address){
-         return player_id_on_bid;
+     mapping(address=>player) map_player;
+     mapping(address=>team) map_team;
+     
+     function playerRegistration(address player_address,uint _base_price, uint _age, string memory _player_type) public{
+        require(map_player[player_address].player_id == address(0), "Player Already Registered");
+        map_player[player_address].player_id = player_address;
+        map_player[player_address].base_price = _base_price;
+        map_player[player_address].age = _age;
+        map_player[player_address].player_type = _player_type;
      }
+     
+     function player_details(address _return_player_id) public view returns(address,uint,uint,string memory){
+        require(map_player[_return_player_id].player_id != address(0), "No Player Found");
+        return (map_player[_return_player_id].player_id, map_player[_return_player_id].base_price, map_player[_return_player_id].age, map_player[_return_player_id].player_type);
+     }
+     
+     function get_player_id(address _return_player_id) public view returns(address){
+         return map_player[_return_player_id].player_id;
+     }
+     
+     function get_player_base_price(address _return_player_id) public view returns(uint){
+         return map_player[_return_player_id].base_price;
+     }
+     
+     function get_player_age(address _return_player_id) public view returns(uint){
+         return map_player[_return_player_id].age;
+     }
+     
+     function get_player_type(address _return_player_id) public view returns(string memory){
+         return map_player[_return_player_id].player_type;
+     }
+     
+     
+     function teamRegistration(address team_address,string memory _team_name) public{
+        require(map_team[team_address].team_id == address(0), "Team Already Registered");
+        map_team[team_address].team_id = team_address;
+        map_team[team_address].player_count =0;
+        map_team[team_address].team_name = _team_name;
+     }
+     
+     
+     function team_details(address _return_team_id) public view returns(address,uint,string memory){
+        require(map_team[_return_team_id].team_id != address(0), "No Team Found");
+        return (map_team[_return_team_id].team_id, map_team[_return_team_id].player_count, map_team[_return_team_id].team_name);
+     }
+     
+     function get_team_id(address _return_team_id) public view returns(address){
+         return map_team[_return_team_id].team_id;
+     }
+     
+     function get_player_count(address _return_team_id) public view returns(uint){
+         return map_team[_return_team_id].player_count;
+     }
+     
+     function get_team_name(address _return_team_id) public view returns(string memory){
+         return map_team[_return_team_id].team_name;
+     }
+     
+     function get_team_players_selected(address _return_team_id) public view returns(address){
+        uint i;
+        require(count == 0, "Players Not Selected Yet");
+        for(i=0;i<=count;i++)
+            {
+                return map_team[_return_team_id].team_players[i];
+            }
+     }
+     
+     function set_selected_player(address team_id ,address _selected_player_id) public{
+        map_team[team_id].team_players.push(_selected_player_id);
+        count+=1;
+     }
+     
+ 
 }
