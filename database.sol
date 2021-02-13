4894a4d14a86b6dba5adfa2db1231612e2cc6e2a
Viraj Shelar <55134977+shelarvs@users.noreply.github.com>
2021-02-13 17:30:32 +0530

    Update database.sol

--- database.sol
+++ database.sol
@@ -19,26 +19,28 @@ contract database{
      mapping(address=>player) map_player;
      mapping(address=>team) map_team;
      
-     function playerRegistration(uint _base_price, uint _age, string memory _player_type) public{
-        
-        map_player[msg.sender].player_id = msg.sender;
-        map_player[msg.sender].base_price = _base_price;
-        map_player[msg.sender].age = _age;
-        map_player[msg.sender].player_type = _player_type;
+     function playerRegistration(address player_address,uint _base_price, uint _age, string memory _player_type) public{
+        require(map_player[player_address].player_id == address(0), "Player Already Registered");
+        map_player[player_address].player_id = player_address;
+        map_player[player_address].base_price = _base_price;
+        map_player[player_address].age = _age;
+        map_player[player_address].player_type = _player_type;
      }
      
-     function teamRegistration(string memory _team_name) public{
-        
-        map_team[msg.sender].team_id = msg.sender;
-        map_team[msg.sender].player_count =0;
-        map_team[msg.sender].team_name = _team_name;
+     function teamRegistration(address team_address,string memory _team_name) public{
+        require(map_team[team_address].team_id == address(0), "Team Already Registered");
+        map_team[team_address].team_id = team_address;
+        map_team[team_address].player_count =0;
+        map_team[team_address].team_name = _team_name;
      }
      
      function player_details(address _return_player_id) public view returns(address,uint,uint,string memory){
+        require(map_player[_return_player_id].player_id != address(0), "No Player Found");
         return (map_player[_return_player_id].player_id, map_player[_return_player_id].base_price, map_player[_return_player_id].age, map_player[_return_player_id].player_type);
      }
      
      function team_details(address _return_team_id) public view returns(address,uint,string memory){
+        require(map_team[_return_team_id].team_id != address(0), "No Team Found");
         return (map_team[_return_team_id].team_id, map_team[_return_team_id].player_count, map_team[_return_team_id].team_name);
      }
  
